6433e5f5fb61a7aa4664372ab8c217498fbb9dbe
dwd <61483125+p3ddd@users.noreply.github.com>
2022-04-10 09:41:58 +0800

    10

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 chmod +x drone-golang-example
 nohup ./drone-golang-example > web.log 2>&1 &
 echo "项目已启动"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 id=$(lsof -i:7007|grep drone-golang-example|awk 'NR==1{print $2}')
 if [ ! ${id} ]
 then
