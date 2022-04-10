b50af6603ef9d8321f97a54019a905156730de48
dwd <61483125+p3ddd@users.noreply.github.com>
2022-04-10 10:10:29 +0800

    15

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!\n自动部署成功\n14")
+	fmt.Fprintf(w, "Hello world!\n自动部署成功\n15")
 }
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":7007", nil)
+	err := http.ListenAndServe(":7007", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 # NR==1 第一行
 # print $2 输出第二个参数
-id=$(lsof -i:7007|/bin/grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox} drone-golang-example|awk 'NR==1{print $2}')
+id=$(ps aux|/bin/grep drone-golang-example|awk 'NR==1{print $2}')
 if [ ! ${id} ]
 then
     echo "项目未启动"
